44a47259bba665b3d9fbc0bf7a71b442edf8c350
Anders Kornkven <anders.kornkven@revature.net>
2021-11-16 14:59:14 -0700

    Finished challenge

--- force-app/main/default/lwc/accountEditForm/accountEditForm.js
+++ force-app/main/default/lwc/accountEditForm/accountEditForm.js
@@ -1,5 +1,6 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
+import ACCOUNT_NAME from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import PHONE_FIELD from '@salesforce/schema/Account.Phone';
 import SLA_FIELD from '@salesforce/schema/Account.SLA__c';
@@ -7,12 +8,14 @@ import SLA_EXPIRATION_FIELD from '@salesforce/schema/Account.SLAExpirationDate__
 import SLA_SERIAL_NUMBER_FIELD from '@salesforce/schema/Account.SLASerialNumber__c';
 import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 
-import { showToastEvent } from 'lightning/platformShowToastEvent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class AccountEditForm extends LightningElement {
-    recordId;
-    objectApiName;
-
+    @api recordId;
+    @api objectApiName;
+    @track readOnly = "false";
+    
+    objectApiName = ACCOUNT_NAME;
     name = NAME_FIELD;
     phone = PHONE_FIELD;
     website = WEBSITE_FIELD;
@@ -21,11 +24,19 @@ export default class AccountEditForm extends LightningElement {
     slaSerialNumber = SLA_SERIAL_NUMBER_FIELD;
 
     handleSuccess() {
-        const toastEvent = new showToastEvent({
+        const toastEvent = new ShowToastEvent({
             title: 'Success',
             message: 'Record saved.',
             variant: 'success'
+
         });
         this.dispatchEvent(toastEvent);
+        
+        if (this.readOnly == false){
+            this.readOnly = true;
+        }
+        else{
+            this.readOnly = false;
+        }
     }
 }
